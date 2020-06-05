323c7101b29d7710784ec6689419f5d67e899822
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-05 13:26:59 +0000

    initial commit

---
+++ fab_by_tiny/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fab_by_tiny project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fab_by_tiny.settings')
+
+application = get_asgi_application()
