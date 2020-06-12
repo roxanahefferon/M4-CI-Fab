a62875c4abff8f4e7ffd907961f416aca716f56c
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-12 13:15:32 +0000

    set up a view for catalogue, created a url path and added to the proyect level urls file

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_catalogue(request):
+    """ View to show all products in catalogue, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'catalogue/products.html', context)
