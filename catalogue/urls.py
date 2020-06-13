059bfedbc677002f93778749a3917e44ae1d195f
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-13 10:38:01 +0000

    created a view, url and template for single product - product_description.html

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_catalogue, name='catalogue')
+    path('', views.all_catalogue, name='catalogue'),
+    path('<product_id>', views.product_description, 
+        name='product_description'),
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_catalogue(request):
     }
 
     return render(request, 'catalogue/catalogue.html', context)
+
+
+def product_description(request, product_id):
+    """ View to show a single product from catalogue """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'catalogue/product_description.html', context)
