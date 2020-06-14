12f0600c1123395d02f37c5b6264053667e42414
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-14 17:30:46 +0000

    worked on shopping basket contexts and add item

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def basket(request):
     """ View to return shopping basket page """
 
     return render(request, 'basket/basket.html')
+
+def add_to_basket(request, item_id):
+    """ View to add a quantity of a specified product to the basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
