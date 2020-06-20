354394e8467841218ed48090d0677b74d060723f
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-20 10:52:51 +0000

    view to handle update and remove actions in basket.html

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def basket(request):
@@ -33,3 +33,42 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+    def update_basket(request, item_id):
+        """ View to update the quantity of a specified product """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size'] 
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[item_id]['items_by_size'][size] = quantity
+            messages.success(request,
+                             (f'Updated size {size.upper()} '
+                              f'{product.name} quantity to '
+                              f'{basket[item_id]["items_by_size"][size]}'))
+        else:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your shopping basket'))
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {basket[item_id]}'))
+        else:
+            basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your shopping basket'))
+
+    request.session['basket'] = basket
+    return redirect(reverse('basket'))
