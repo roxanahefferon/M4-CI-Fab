9ee8aaa0c39f0aa169b429f24d031c10a641b542
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-25 23:20:32 +0000

    created view for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "There are no items in your shopping basket at the moment")
+        return redirect(reverse('catalogue'))
+
+        order_form = OrderForm()  # instance of the order form
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+        }
+
+        return render(request, template, context )
