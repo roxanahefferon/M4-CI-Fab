fdabfca4e6a27be3b8f2f1e31553f7fb88f05738
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-26 15:19:11 +0000

    handled card validation error message

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,8 +5,8 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('first_name', 'last_name', 'email', 
-                  'phone_number', 'street_address_1', 
+        fields = ('first_name', 'last_name', 'email',
+                  'phone_number', 'street_address_1',
                   'street_address_2', 'town', 'postcode',
                   'county',)
 
