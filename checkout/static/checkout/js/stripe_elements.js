6184ab39fd4603afec4fc98304c54ea76bf854e3
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-26 22:38:28 +0000

    added event listener to pymt form submit

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,9 +1,7 @@
 /*
-    Core logic/payment flow for this comes from here:
+    core logic/payment flow from stripe documentation:
     https://stripe.com/docs/payments/accept-a-payment
     
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
 */
 
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
@@ -28,7 +26,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Realtime validation errors on the card element
+// validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -42,4 +40,37 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// form submit from stripe documentation
+var form = document.getElementById('card-payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // shows error message to customer
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // re enable to fix card error
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // shows payment success message 
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
