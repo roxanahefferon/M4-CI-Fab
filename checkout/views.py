fb0297f653c49f064abc4f1582057b577c6749f3
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-26 21:40:35 +0000

    fixed stripe.error.AuthenticationError: You did not provide an API key, keys added to gitpod settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from basket.contexts import basket_contents
@@ -10,20 +11,35 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
-        messages.error(request, "There are no items in your basket at the moment")
+        messages.error(request, "There are no items in your basket")
         return redirect(reverse('catalogue'))
 
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Missing stripe public key')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51GyHQQGxUCcWt6YF7OBZTLwyOVU5oJCgwt0Jiu2HQqFMuOa2FwtCRDLLB2AdNAW54SXj9MI86Xeb23VaGHXXninz00ghx2DAtG',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- fab_by_tiny/settings.py
+++ fab_by_tiny/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from os import environ
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -168,3 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
