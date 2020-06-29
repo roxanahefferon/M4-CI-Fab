6d252bd10f164ad8f36b4e47e579cffd01d1ee80
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-29 11:14:40 +0000

    generated new secret key, added to heroku config vars and in settings.py, changed debug to development

--- fab_by_tiny/settings.py
+++ fab_by_tiny/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'biv+!$wsmsoy#eh1aqgv9epwx9agkh1fmus76(%1%w0a!pk9$2'
+SECRET_KEY = 'os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fab-by-tiny.herokuapp.com', 'localhost']
 
