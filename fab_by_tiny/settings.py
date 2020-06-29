5719e48a89d8b9b875910643a96f7c7214749681
roxanahefferon <roxana.hefferon@gmail.com>
2020-06-29 10:07:39 +0000

    disabled collectstatic, heroku app and localhost as allowed hosts

--- fab_by_tiny/settings.py
+++ fab_by_tiny/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'biv+!$wsmsoy#eh1aqgv9epwx9agkh1fmus76(%1%w0a!pk9$2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fab-by-tiny.herokuapp.com', 'localhost']
 
 
 # Application definition
